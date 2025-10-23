f12a2676bf3c44e431f949abf0b786346070a135
MINH MAN <MINH MAN@DESKTOP-GKDUPPR>
2025-10-23 10:11:03 +0700

    Minh Man

---
+++ src/main/java/com/mycompany/quanlyspa/QuanLySpa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quanlyspa;
+
+/**
+ *
+ * @author MINH MAN
+ */
+public class QuanLySpa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
