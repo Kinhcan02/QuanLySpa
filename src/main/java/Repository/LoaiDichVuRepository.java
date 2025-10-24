4a6664329595b4bd5a51fb239b29b82e9cb613b0
Thien <dangthien2005@gmail.com>
2025-10-24 10:50:52 +0700

    First Commit

--- src/main/java/Repository/LoaiDichVuRepository.java
+++ src/main/java/Repository/LoaiDichVuRepository.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Service;
+package Repository;
 
 /**
  *
  * @author ASUS
  */
-public class Auth {
+public class LoaiDichVuRepository {
     
 }
