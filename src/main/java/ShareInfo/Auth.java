ef29e007dc4d6fd4ef87469fda21fc389b31321b
Thien <dangthien2005@gmail.com>
2025-10-24 12:54:57 +0700

    Auth

--- src/main/java/ShareInfo/Auth.java
+++ src/main/java/ShareInfo/Auth.java
@@ -9,5 +9,23 @@ package ShareInfo;
  * @author ASUS
  */
 public class Auth {
+     private Auth() {
+    }
     
+    public static  int maTaiKhoan = -1;
+    public static  String tenDangNhap= null;
+    public static  String matKhau  = null;
+    public static  String loaiNguoiDung = null;
+    public static  boolean dangNhap = false;
+    
+    public static void clear(){
+        maTaiKhoan= -1;
+        tenDangNhap = null;
+        matKhau = null;
+        loaiNguoiDung = null;
+        dangNhap = false;
+    }
+    public static boolean isLogin() {
+        return dangNhap;
+    }
 }
